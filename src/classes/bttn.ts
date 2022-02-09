27a8fde10cef59c2f0ad653da0b784946a20c5a6
Florian Porada <florian.porada@gmail.com>
2022-02-09 12:14:32 +0100

    feat: add new tailwind config

---
+++ src/classes/bttn.ts
@@ -0,0 +1,1 @@
+export default 'px-5 py-2 rounded-full bg-red text-white leading-[24px] inline-flex items-center';
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import button from './classes/button';
 import dummyClass from './classes/dummyClass';
+import bttn from './classes/bttn';
 
 export default {
   button: button,
   dummyClass: dummyClass,
+  bttn: bttn,
 };
