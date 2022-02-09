da7c31bf8cf0be9d2e65f38c84d952546bd2fdae
Florian Porada <florian.porada@gmail.com>
2022-02-09 10:41:17 +0100

    feat: add svg icon files

--- ICONS.md
+++ ICONS.md
@@ -16,6 +16,17 @@ initIcons();
 console.log(icons)
 ```
 
+
+```js
+const icons = require('@47nordmedia/k2-systems-ui-library/icons');
+
+// init icon library
+icons.initIcons();
+
+// list of all available icons
+console.log(icons.icons)
+```
+
 ## Usage
 
 Use a `span` element with the classes `icon icon-<iconname>` to add a SVG icon (handled by javascript)
--- src/icons.ts
+++ src/icons.ts
@@ -1,5 +1,24 @@
-const icons: { [key: string]: { svg: any } } = {
+const icons: { [key: string]: { svg: string } } = {
+  'arrow-right-24': { svg: require('./icons/arrow_right_24.svg') },
+  'cables-64': { svg: require('./icons/cables_64.svg') },
+  'checked-24': { svg: require('./icons/checked_24.svg') },
+  'connected-64': { svg: require('./icons/connected_64.svg') },
+  'droplets-64': { svg: require('./icons/droplets_64.svg') },
+  'green-64': { svg: require('./icons/green_64.svg') },
+  'happy-64': { svg: require('./icons/happy_64.svg') },
+  'layers-64': { svg: require('./icons/layers_64.svg') },
+  'mail-24': { svg: require('./icons/mail_24.svg') },
+  'play-24': { svg: require('./icons/play_24.svg') },
+  'plus-24': { svg: require('./icons/plus_24.svg') },
+  'power-48': { svg: require('./icons/power_48.svg') },
+  'power-64': { svg: require('./icons/power_64.svg') },
+  'search-slim-24': { svg: require('./icons/search_slim_24.svg') },
   'secured-64': { svg: require('./icons/secured_64.svg') },
+  'turnoff-64': { svg: require('./icons/turnoff_64.svg') },
+  'user-slim-24': { svg: require('./icons/user_slim_24.svg') },
+  'world-48': { svg: require('./icons/world_48.svg') },
+  'world-64': { svg: require('./icons/world_64.svg') },
+  'world-slim-24': { svg: require('./icons/world_slim_24.svg') },
 };
 
 function initIcons() {
