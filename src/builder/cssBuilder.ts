fab0dea026800f6ae52722eea2a12a654129d1d6
Florian Porada <florian.porada@gmail.com>
2022-04-10 17:12:22 +0200

    feat: add scss overwrites for shopware themes

--- src/builder/cssBuilder.ts
+++ src/builder/cssBuilder.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { dashize } from './helper';
+import tailwindConfig from '../tailwind.config.js';
 
 const filePath = './dist';
 
@@ -31,6 +32,56 @@ export function createCssFile(classesObject: { [key: string]: string }): void {
   try {
     fs.mkdirSync(filePath, { recursive: true });
     fs.writeFileSync(`${filePath}/main.css`, cssClasses.join('\n'));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export function createScssOverwrites() {
+  const {
+    theme: { extend },
+  } = tailwindConfig;
+  const content = `
+$sw-color-brand-primary: ${extend.colors.red};
+$sw-color-brand-secondary: ${extend.colors.black};
+$sw-color-price: ${extend.colors.darkGrey};
+
+$sw-color-success: ${extend.colors.forestGreenAccent};
+$sw-color-info: ${extend.colors.seaBlueAccent};
+$sw-color-warning: ${extend.colors.orangeAccent};
+$sw-color-danger: ${extend.colors.brownAccent};
+
+$sw-background-color: ${extend.colors.white};
+$sw-text-color:  ${extend.colors.black};
+$sw-headline-color: ${extend.colors.darkGrey};
+$sw-border-color: ${extend.colors.lightGrey};
+
+$sw-color-buy-button: ${extend.colors.red};
+$sw-color-buy-button-text: ${extend.colors.white};
+
+//fonts
+$sw-font-family-base: ${tailwindConfig.theme.extend.fontFamily.body
+    .map((el) => (el === 'sans-serif' ? el : `'${el}'`))
+    .join(', ')};
+$sw-font-family-headline: ${tailwindConfig.theme.extend.fontFamily.body
+    .map((el) => (el === 'sans-serif' ? el : `'${el}'`))
+    .join(', ')};
+
+//logos
+$sw-logo-default: url('logo.png') !default;
+$sw-logo-default-sm: url('logo.png') !default;
+$sw-logo-default-md: url('logo.png') !default;
+$sw-logo-default-lg: url('logo.png') !default;
+$sw-logo-default-xl: url('logo.png') !default;
+
+$sw-logo-share: url('logo-share.png') !default;
+$sw-logo-favicon: url('logo-favicon.png') !default;
+$sw-logo-app-icon: url('logo-app-icon.png') !default;
+  `;
+
+  try {
+    fs.mkdirSync(filePath, { recursive: true });
+    fs.writeFileSync(`${filePath}/shopwareOverwrites.scss`, content);
   } catch (error) {
     console.log(error);
   }
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     extend: {
       fontFamily: {
         display: ['Inter', 'system-ui', 'sans-serif'],
-        body: ['Inter', 'system-ui', 'sans-serif'],
+        body: ['Halvar', 'Inter', 'system-ui', 'sans-serif'],
         halvar: ['Halvar'],
       },
       colors: {
@@ -42,110 +42,110 @@ module.exports = {
         '0xs': ['0rem', '0rem'],
         '2xs': [
           '0.813rem',
-           {
+          {
             letterSpacing: '0.005em',
             lineHeight: '1.5rem',
-          }
+          },
         ],
-        'xs': [
+        xs: [
           '1rem',
           {
             letterSpacing: '0.005em',
             lineHeight: '1.5rem',
           },
         ],
-        'sm': [
+        sm: [
           '1.125rem',
           {
             letterSpacing: '-0.015em',
             lineHeight: '1.5rem',
           },
         ],
-        'base': [
+        base: [
           '1.313rem',
           {
             letterSpacing: '-0.015em',
             lineHeight: '2rem',
           },
         ],
-        "md": [
+        md: [
           '1.5rem',
           {
             letterSpacing: '-0.025em',
             lineHeight: '2rem',
           },
         ],
-        "lg": [
+        lg: [
           '1.875rem',
           {
             letterSpacing: '-0.025em',
             lineHeight: '2.25rem',
           },
         ],
-        "xl": [
+        xl: [
           '2.5rem',
           {
             letterSpacing: '-0.025em',
             lineHeight: '3rem',
           },
         ],
-        "2xl": [
+        '2xl': [
           '2.625rem',
           {
             letterSpacing: '-0.025em',
             lineHeight: '3.25rem',
           },
         ],
-        "3xl": [
+        '3xl': [
           '3rem',
           {
             letterSpacing: '-0.025em',
             lineHeight: '3.5rem',
           },
         ],
-        "4xl": [
+        '4xl': [
           '3.5rem',
           {
             letterSpacing: '-0.025em',
             lineHeight: '4.25rem',
           },
         ],
-        "5xl": [
+        '5xl': [
           '4rem',
           {
             letterSpacing: '-0.025em',
             lineHeight: '4rem',
           },
         ],
-        "6xl": [
+        '6xl': [
           '4.5rem',
           {
             letterSpacing: '-0.025em',
             lineHeight: '5.5rem',
           },
         ],
-        "7xl": [
+        '7xl': [
           '5.5rem',
           {
             letterSpacing: '-0.025em',
             lineHeight: '5.5rem',
           },
         ],
-        "8xl": [
+        '8xl': [
           '6rem',
           {
             letterSpacing: '-0.025em',
             lineHeight: '6rem',
           },
         ],
-        "9xl": [
+        '9xl': [
           '7rem',
           {
             letterSpacing: '-0.025em',
             lineHeight: '7rem',
           },
         ],
-        "10xl": [
+        '10xl': [
           '9rem',
           {
             letterSpacing: '-0.025em',
