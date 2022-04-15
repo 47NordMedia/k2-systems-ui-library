a49313c4637835673957023ffa0d1db34a8d6613
sifat88 <rakibulislam8sifat@gmail.com>
2022-04-15 18:45:39 +0600

    missing icons fix

--- src/iconList.ts
+++ src/iconList.ts
@@ -2,6 +2,8 @@ const icons: { [key: string]: { svg: string } } = {
   '1-48': { svg: require('./icons/1-48.svg') },
   '2-48': { svg: require('./icons/2-48.svg') },
   '3-48': { svg: require('./icons/3-48.svg') },
+  '4-48': { svg: require('./icons/4-48.svg') },
+  '5-48': { svg: require('./icons/5-48.svg') },
   '15min-64': { svg: require('./icons/15min.svg') },
   'arrow-circle': { svg: require('./icons/arrow-circle.svg') },
   'arrow-right-14': { svg: require('./icons/arrow-right.svg') },
